f86f602cf34dbcb4fd7c9dbd92e982bd70861ab7
caasi Huang <caasi.igd@gmail.com>
2014-04-22 11:26:13 +0800

    sort of init commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 (function(){
-  console.log('foobar');
+  var data;
+  data = {
+    image: 'img/kuma.png',
+    mask: void 8,
+    sprite: {
+      width: 24,
+      height: 32
+    },
+    relations: [void 8, void 8, void 8, 1, void 8, void 8, void 8, 5, void 8, void 8, void 8, 9, void 8, void 8, void 8, 13, void 8]
+  };
+  console.log(data);
 }).call(this);
---
+++ src/ls/data.ls
@@ -0,0 +1,24 @@
+data =
+  image: 'img/kuma.png'
+  mask: void
+  sprite:
+    width:  24
+    height: 32
+  relations:
+    * void
+      void
+      void
+      1
+      void
+      void
+      void
+      5
+      void
+      void
+      void
+      9
+      void
+      void
+      void
+      13
+      void
